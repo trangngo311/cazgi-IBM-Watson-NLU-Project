07aab845451a2ee3626ad271d13bc2757ec1516a
trangngo311 <tranghthngo@gmail.com>
2021-07-29 00:56:40 +0000

    finish 3 steps

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return NaturalLanguageUnderstandingV1;
+}
+
 const app = new express();
 
 app.use(express.static('client'))
@@ -26,6 +46,8 @@ app.get("/text/emotion", (req,res) => {
 app.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
+
+
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
